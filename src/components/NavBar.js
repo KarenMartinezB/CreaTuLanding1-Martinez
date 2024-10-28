38fd34b2193a0953e3518fcee98370ab537e971b
KarenMartinezB <bloemermartinezkaren@gmail.com>
2024-10-28 20:35:34 -0300

    Changed NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function NavBar() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#home">Inicio</a></li>
+                <li><a href="#products">Productos</a></li>
+                <li><a href="#about">Nosotros</a></li>
+                <li><a href="#contact">Contacto</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
