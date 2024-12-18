a66b10304edfccc994c4054cb2a4ebff9f285130
KarenMartinezB <bloemermartinezkaren@gmail.com>
2024-12-18 17:20:01 -0300

    Changed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,14 +7,8 @@ function NavBar() {
     <nav className="navbar">
       <div className="container">
         <Link className="logo" to="/">
-          CookiBoki 📚 Book Store
+        CookiBooki📚 Book Store
         </Link>
-        <div className="menu">
-          <Link to="/category/best-sellers">Best Sellers</Link>
-          <Link to="/category/fiction">Fiction</Link>
-          <Link to="/category/non-fiction">Non-fiction</Link>
-          <Link to="/category/children">Children</Link>
-        </div>
         <div className="cart">
           <Link to="/cart">🛒</Link>
         </div>
