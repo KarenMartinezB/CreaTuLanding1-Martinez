5a3eee566628addafe892a5ffa7e28ecf089f80a
KarenMartinezB <bloemermartinezkaren@gmail.com>
2024-12-18 16:34:45 -0300

    Changed detail container

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../firebaseConfig";
 import { useParams } from "react-router-dom";
+import "../styles/ItemDetailContainer.css";
 
 function ItemDetailContainer() {
   const { id } = useParams();
@@ -20,13 +21,16 @@ function ItemDetailContainer() {
   }, [id]);
 
   return book ? (
-    <div className="detail-container">
-      <img src={book.image} alt={book.title} />
-      <div className="details">
-        <h2>{book.title}</h2>
-        <p>{book.synopsis}</p>
-        <p>Price: ${book.price}</p>
-        <p>Reviews: ⭐ {book.reviews}</p>
+    <div className="item-detail-container">
+      <div className="item-detail">
+        <img src={book.image} alt={book.title} className="detail-image" />
+        <div className="detail-text">
+          <h2>{book.title}</h2>
+          <p><strong>Author:</strong> {book.author}</p>
+          <p><strong>Price:</strong> ${book.price.toLocaleString()}</p>
+          <p><strong>Synopsis:</strong> {book.synopsis}</p>
+          <p><strong>Reviews:</strong> ⭐ {book.reviews}</p>
+        </div>
       </div>
     </div>
   ) : (
